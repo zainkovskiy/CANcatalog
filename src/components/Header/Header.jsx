77d6a4b99932dbb96c2ac50caf0c7a08e2fbf572
zainkovskiyaa <Piton1808@gmail.com>
2022-08-15 16:37:50 +0700

    add button attention

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Button } from "@mui/material"; 
 
 import './Header.scss';
 
 export function Header () {
+
+  const openAttention = () => {
+    BX.SidePanel.Instance.open(`https://crm.centralnoe.ru/attention/?reqNumber=&dealId=0&source=`, { animationDuration: 300, width: document.getElementById('root').clientWidth })
+  }
+
   return (<div className='header'>
             <img className='header__logo' src="https://crm.centralnoe.ru/dealincom/assets/logo_can.jpg" alt="logo"/>
+            <Button
+              size="small"
+              variant='outlined'
+              color='error'
+              onClick={openAttention}
+            >
+              сообщить о проблеме
+            </Button>
           </div>)
 }
