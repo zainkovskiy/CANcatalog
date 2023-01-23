4ffe27b1d269ca90ce5566ae7b6faf3dad6ef87e
Anton Zainkovskiy <Piton1808@gmail.com>
2023-01-23 10:19:03 +0100

    change version

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     path: path.resolve(__dirname, 'src', 'index.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist', 'v1.3.6'),
-    filename: 'bundle-v1.3.6.js',
+    path: path.resolve(__dirname, 'dist', 'v1.3.7'),
+    filename: 'bundle-v1.3.7.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -64,7 +64,7 @@ module.exports = {
       fiename: 'index.html',
     }),
     new miniCssExtractPlugin({
-      filename: 'main-v1.3.6.css',
+      filename: 'main-v1.3.7.css',
     }),
   ],
 };
